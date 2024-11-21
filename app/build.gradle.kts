e31ad7075a898ef26a1b2b03b2c17f359e6aa7b0
Muhammad Ishfaq <ishfaqcompk6@gmail.com>
2024-11-22 00:19:55 +0500

    fetch timeout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.remote.config"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.remote.config"
